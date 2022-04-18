0f31275a16716e0ce105e3f47f509d00187e9a42
zahidmiran <zahidmiron599@gmail.com>
2022-04-18 19:05:07 +0600

    about section done

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import './About.css'
 
 const About = () => {
     return (
-        <div>
-            About
-        </div>
+        <section className='px-4 py-32 mx-auto max-w-7xl'>
+            <h2 className="title">About me</h2>
+            <p className='text-center text-2xl font-bold pb-2 text-orange-400'>i am</p>
+
+            <div className='containe about-parent'>
+
+                <div>
+                    <div className='personal-photo  bg-gray-200 rounded-lg'>
+                        <img
+                            src='about.jpg'
+                            alt=''
+                        />
+                    </div>
+                </div>
+
+                <div>
+                    <p className='text-center'>
+                        I am Jawad and I'm a
+                    </p>
+                    <h1 className='text-center'>
+                        Web Developer.
+                    </h1>
+                    <p className='mb-5 text-base text-left text-gray-800 md:text-xl'>
+                        <p>Self-motivated and confident Web Developer. Able to do any web and programming related task by handling difficulties smoothly in any given time. Have a reasonable skill on Front-End and Back-End development.</p>
+                    </p>
+
+                </div>
+            </div>
+        </section>
     );
 };
 
--- src/components/Services/Service.js
+++ src/components/Services/Service.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Button, ButtonGroup, Card } from 'react-bootstrap';
 import { Link, useNavigate } from 'react-router-dom';
+import './Service.css'
 
 const Service = ({ service }) => {
     const { name, img, price, description, id } = service;
@@ -10,7 +11,7 @@ const Service = ({ service }) => {
         navigate(`/checkout/${name}`);
     }
     return (
-        <div className='service'>
+        <div className='service container '>
             <Card style={{ width: '18rem' }}>
                 <Card.Img variant="top" src={img} />
                 <Card.Body>
